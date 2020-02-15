47bf64c862c7bfb2e8db9f067f412098421be0e8
logan <lo4952@vt.edu>
2020-02-15 17:16:47 -0500

    Changed text-to-speech method

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-# Requires PyAudio, wolframalpha, cryptography
+# Requires PyAudio, wolframalpha, cryptography, ibm_watson, watson_text_talker
 
-from ServerKeys import apikey, url, app_id
+# Using code from the watson_text_talker github project
+
+from ServerKeys import *
+from watson_text_talker import *
 
 from ibm_watson import TextToSpeechV1
 from ibm_watson.websocket import SynthesizeCallback
@@ -19,68 +22,14 @@ def get_answer(query_text):
 	answer = next(res.results).text 
 	return answer
 
-class Play(object):
-    # Wrapper to play the audio in a blocking mode
-    def __init__(self):
-        self.format = pyaudio.paInt16
-        self.channels = 1
-        self.rate = 22050
-        self.chunk = 1024
-        self.pyaudio = None
-        self.stream = None
+config = TT_Config()
+config.API_KEY=apikey
+config.API_URL=url
+config.TTS_Voice = 'en-US_MichaelVoice'
+config.CACHE_DIRECTORY = 'custom_cache'
+config.INITIALIZATION_DELAY = 2
 
-    def start_streaming(self):
-        self.pyaudio = pyaudio.PyAudio()
-        self.stream = self._open_stream()
-        self._start_stream()
-
-    def _open_stream(self):
-        stream = self.pyaudio.open(
-            format=self.format,
-            channels=self.channels,
-            rate=self.rate,
-            output=True,
-            frames_per_buffer=self.chunk,
-            start=False
-        )
-        return stream
-
-    def _start_stream(self):
-        self.stream.start_stream()
-
-    def write_stream(self, audio_stream):
-        self.stream.write(audio_stream)
-
-    def complete_playing(self):
-        self.stream.stop_stream()
-        self.stream.close()
-        self.pyaudio.terminate()
-
-class MySynthesizeCallback(SynthesizeCallback):
-    def __init__(self):
-        SynthesizeCallback.__init__(self)
-        self.play = Play()
-
-    def on_connected(self):
-        #print('Opening stream to play')
-        self.play.start_streaming()
-
-    def on_error(self, error):
-    	pass
-        #print('Error received: {}'.format(error))
-
-    def on_timing_information(self, timing_information):
-    	pass
-        #print(timing_information)
-
-    def on_audio_stream(self, audio_stream):
-        self.play.write_stream(audio_stream)
-
-    def on_close(self):
-        #print('Completed synthesizing')
-        self.play.complete_playing()
-
-test_callback = MySynthesizeCallback()
+text_talker = TextTalker(config=config)
 
 def get_time():
 	ts = time.gmtime()
@@ -128,7 +77,7 @@ while 1:
 	str_time = get_time()
 	print("[" + str_time + "] Speaking Question: ", query_str)
 
-	service.synthesize_using_websocket(query_str, test_callback, accept='audio/wav', voice="en-US_AllisonVoice")
+    text_talker.say(query_str)
 
 	str_time = get_time()
 	print("[" + str_time + "] Sending question to Wolframalpha: ", query_str)
